08a5fd3641416533eea671fba2cd8d0f641df988
qunzhongcheng@creditease.cn <qunzhongcheng@creditease.cn>
2021-03-23 14:52:41 +0800

    add description

---
+++ prettierrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  // 总是使用单引号
+  // singleQuote: true,
+
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+  ],
+
+
+  
+  // 配置标签换行格式
+  // 与 htmlWhitespaceSensitivity 一起配置才能生效
+  // htmlWhitespaceSensitivity: "css" | "strict" | "ignore"
+  jsxBracketSameLine: true,
+  htmlWhitespaceSensitivity: "ignore",
+  requirePragma: false,
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
+};
--- vue-styles.md
+++ vue-styles.md
@@ -1,5 +1,26 @@
 
-## WEB APP STYLE
+## WEB APP VUE STYLE
+
+### 名称的写法
+
+- UpperCamelCase / PascalCase
+所有单词首字符都大写
+
+- lowerCamelCase / camelCase
+第一个单词首字母小写，其余单词首字母都大写
+
+- kebab-case
+短横线分隔命名
+
+#### 定义组件的名称
+
+定义组件时使用UpperCamelCase命名，`MyComponentName`，在使用组件时用`<MyComponentName></MyComponentName>`。
+
+> 尽管直接在DOM（非模版）中使用使用时只有kebab-case有效，`<my-component-name></my-component-name>`
+
+但是，在工程化项目中所有场景都在模版下，UpperCamelCase的优势就会显现出来
+- 自定义组件显示明了
+- 组件搜索方便快捷
 
 ### 定义组件文件的名称
 
